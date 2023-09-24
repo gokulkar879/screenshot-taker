31b012e47e31b97ee8bff47d5442e43f085acff4
gokulkar879 <gokulkarki987654@gmail.com>
2023-09-24 14:01:05 +0530

    created the repo

---
+++ __tests__/script.js
@@ -0,0 +1,20 @@
+const puppeteer = require("puppeteer");
+
+describe("take scrrenshot", () => {
+    let browser;
+    let page;
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+    })
+
+    it("takes scrrenshot", async () => {
+        await page.goto('http://localhost:5000');
+        const image = await page.screenshot({ path: 'README.md' });
+    })
+
+    afterAll(async () => {
+        await browser.close();
+    });
+})
+
